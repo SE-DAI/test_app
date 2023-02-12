054361d46e21158948143b47edf0822a6a4dc774
seidai <boc.yui.xysy@gmail.com>
2023-02-13 02:51:33 +0900

    signup-page-create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,23 +3,29 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new
+    if @user.save
+      log_in @user
+      flash[:success] = "新規作成に成功しました。"
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def show
-
   end
 
   def edit
-
   end
 
   def update
-    
   end
 
   def destroy
-    
   end
-
 end
