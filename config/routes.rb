79dd7825ffb19074bbad8e332402f2ef093cb2f4
seidai <boc.yui.xysy@gmail.com>
2023-02-16 01:44:59 +0900

    login-MVC-create

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root "static_pages#top"
-  get  "/signup", to: "users#new"
-  get  "/login", to: "sessions#new"
+  get "/signup", to: "users#new"
+
+  # ログイン機能
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   resources :users do
-
   end
 end
